97ecd5cc85ad4b42597bb37a7b15938e609d7494
abidakhrs <116633931+abidakhrs@users.noreply.github.com>
2025-01-02 11:10:54 +0800

    new

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,5 +40,5 @@ dependencies {
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
     implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
-
+    implementation ("org.osmdroid:osmdroid-android:6.1.13")
 }
